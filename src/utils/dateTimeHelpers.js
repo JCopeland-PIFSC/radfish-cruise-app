87135ba5e27ae39ac0350eccf291f57a541600ea
Jesse Copeland <jesse.copeland@noaa.gov>
2024-10-25 16:59:21 -1000

    Add helper function to set datetime from date and time form inputs

--- src/utils/dateTimeHelpers.js
+++ src/utils/dateTimeHelpers.js
@@ -1,5 +1,5 @@
-import { parseISO, differenceInMinutes } from "date-fns";
-import { formatInTimeZone } from "date-fns-tz";
+import { parseISO, differenceInMinutes, parse, format, set } from "date-fns";
+import { fromZonedTime, formatInTimeZone } from "date-fns-tz";
 
 export function utcToLocalDateTime(utcDateString) {
   if (!utcDateString) return "";
@@ -23,4 +23,24 @@ export function getSoakTime(begTime, endTime) {
   const minutes = totalMinutes % 60;
 
   return `${hours}h ${minutes}m`;
+}
+
+export function combineDateTime(date, time) {
+  // Convert strings to Date objects in system timezone
+  const parsedDate = parse(date, "yyyy-MM-dd", new Date()); // Adjust date format as needed
+  const parsedTime = parse(time, "HH:mm", new Date()); // Adjust time format as needed
+
+  // Combine date and time into a single Date object
+  const combinedDateTime = set(parsedDate, {
+    hours: parsedTime.getHours(),
+    minutes: parsedTime.getMinutes(),
+    seconds: 0,
+  });
+  // Convert the combined Date to UTC
+  const utcDate = fromZonedTime(
+    combinedDateTime,
+    Intl.DateTimeFormat().resolvedOptions().timeZone,
+  );
+
+  return format(utcDate, "yyyy-MM-dd'T'HH:mm:ss'Z'");
 }
