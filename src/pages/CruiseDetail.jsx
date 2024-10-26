1aecc8a55f99fde83b713c06c1bc6641f5130647
Jesse Copeland <jesse.copeland@noaa.gov>
2024-10-25 16:58:08 -1000

    Add new Station form handler

--- src/pages/CruiseDetail.jsx
+++ src/pages/CruiseDetail.jsx
@@ -1,22 +1,27 @@
 import "../index.css";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
-  Title,
   Button,
   Grid,
   Fieldset,
   Tag,
 } from "@trussworks/react-uswds";
 import { StationSummary } from "../components/StationSummary";
+import { StationNew } from "../components/StationNew";
 import { CruiseContext } from "../CruiseContext";
 import { DescriptionListItem } from "../components/DescriptionListItem";
 import { useNavigate } from "react-router-dom";
 import { listValueLookup } from "../utils/listLookup";
 import { setStatusColor } from "../utils/setStatusColor";
+import { combineDateTime } from "../utils/dateTimeHelpers";
+import { post } from "../utils/requestMethods";
+
+const API_BASE_URL = "http://localhost:5000";
 
 function CruiseDetailPage({ data }) {
-  const { cruise, stations } = data;
+  const { cruise, stations: initialStations } = data;
   const {
+    id,
     cruiseName,
     cruiseStatusId,
     vesselName,
@@ -28,13 +33,63 @@ function CruiseDetailPage({ data }) {
   const navigate = useNavigate();
   const { state } = useContext(CruiseContext);
   const { ports, cruiseStatuses } = state;
+  const [enterNewStation, setEnterNewStation] = useState(false);
   const cruiseStatus = listValueLookup(cruiseStatuses, cruiseStatusId);
   const departurePort = listValueLookup(ports, departurePortId);
   const returnPort = listValueLookup(ports, returnPortId);
+  const [stations, setStations] = useState(initialStations);
 
   const handleNavCruisesList = () => {
     navigate("/cruises");
   };
+
+  const handleEnterNewStation = () => {
+    setEnterNewStation(!enterNewStation);
+  };
+
+  const newStationButton = (statusId, newStationToggle) => {
+    if (statusId.toString() === "3" || statusId.toString() === "5") {
+      return <Button disabled>New Station</Button>
+    }
+    return newStationToggle
+      ? <Button onClick={handleEnterNewStation} secondary>Cancel New Station</Button>
+      : <Button onClick={handleEnterNewStation}>New Station</Button>
+  }
+
+  const handleNewStation = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const timeValue = formData.get('beginSetTime');
+    console.log('Selected Time:', timeValue);
+
+    const values = { cruiseId: id };
+
+    for (const [key, value] of formData.entries()) {
+      values[key] = value;
+    }
+    const beginSetDateTime = combineDateTime(values.beginSetDate, values.beginSetTime);
+    const newValues = structuredClone(InitializedStation);
+    newValues.cruiseId = values.cruiseId;
+    newValues.stationName = values.stationName;
+    newValues.location.latitude = values.latitude;
+    newValues.location.longitude = values.longitude;
+    newValues.events.beginSet.timestamp = beginSetDateTime;
+    newValues.events.beginSet.latitude = values.latitude;
+    newValues.events.beginSet.longitude = values.longitude;
+    newValues.events.beginSet.weatherConditions.windSpeedKnots = values.windSpeed;
+    newValues.events.beginSet.weatherConditions.waveHeightMeters = values.waveHeight;
+    newValues.events.beginSet.weatherConditions.visibilityKm = values.visibility;
+    newValues.events.beginSet.weatherConditions.precipitationId = values.precipitationId;
+    newValues.events.beginSet.comments = values.comments;
+
+    // process date time
+    const newStation = await post(`${API_BASE_URL}/stations`, newValues);
+    event.target.reset();
+    setEnterNewStation(!enterNewStation);
+    setStations([newStation, ...stations]);
+  }
 
   return (
     <>
@@ -70,9 +125,11 @@ function CruiseDetailPage({ data }) {
             </Fieldset>
           </Grid>
         </Grid>
-        <Grid row>
+        <Grid row className="flex-justify margin-bottom-1">
           <h2 className="app-sec-header">Stations</h2>
+          {newStationButton(cruiseStatusId, enterNewStation)}
         </Grid>
+        {enterNewStation && <StationNew handleNewStation={handleNewStation} />}
         {stations.length
           ? stations.map((station) => (
             <StationSummary key={station.id} station={station} />
@@ -84,3 +141,26 @@ function CruiseDetailPage({ data }) {
 }
 
 export default CruiseDetailPage;
+
+const InitializedStation = {
+  cruiseId: null,
+  stationName: null,
+  location: {
+    latitude: null,
+    longitude: null
+  },
+  events: {
+    beginSet: {
+      timestamp: null,
+      latitude: null,
+      longitude: null,
+      weatherConditions: {
+        windSpeedKnots: null,
+        waveHeightMeters: null,
+        visibilityKm: null,
+        precipitationId: null,
+      },
+      comments: null
+    },
+  }
+}
