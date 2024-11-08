01b3dfa83adc921fd6526fbc515220d0ee688f46
Jesse Copeland <jesse.copeland@noaa.gov>
2024-11-07 15:48:52 -1000

    Add edit Cruise functionality
    Move newStationButton functionality into body

--- src/pages/CruiseDetail.jsx
+++ src/pages/CruiseDetail.jsx
@@ -5,21 +5,27 @@ import {
   Grid,
   Fieldset,
   Tag,
+  Form,
+  Label,
+  TextInput,
+  DatePicker,
+  Select
 } from "@trussworks/react-uswds";
 import { StationSummary } from "../components/StationSummary";
 import { StationNew } from "../components/StationNew";
 import { CruiseContext } from "../CruiseContext";
 import { DescriptionListItem } from "../components/DescriptionListItem";
 import { useNavigate } from "react-router-dom";
-import { listValueLookup } from "../utils/listLookup";
+import { listValueLookup, CruiseStatus } from "../utils/listLookup";
 import { setStatusColor } from "../utils/setStatusColor";
 import { generateTzDateTime, getLocationTz } from "../utils/dateTimeHelpers";
-import { post } from "../utils/requestMethods";
+import { post, put } from "../utils/requestMethods";
 
 const API_BASE_URL = "http://localhost:5000";
 
 function CruiseDetailPage({ data }) {
-  const { cruise, stations: initialStations } = data;
+  const { cruise: initialCruise, stations: initialStations } = data;
+  const [cruise, setCruise] = useState(initialCruise);
   const {
     id,
     cruiseName,
@@ -33,28 +39,47 @@ function CruiseDetailPage({ data }) {
   const navigate = useNavigate();
   const { state } = useContext(CruiseContext);
   const { ports, cruiseStatuses } = state;
-  const [enterNewStation, setEnterNewStation] = useState(false);
+  const [newStationToggle, setNewStationToggle] = useState(false);
+  const [editCruiseToggle, setEditCruiseToggle] = useState(false);
   const cruiseStatus = listValueLookup(cruiseStatuses, cruiseStatusId);
   const departurePort = listValueLookup(ports, departurePortId);
   const returnPort = listValueLookup(ports, returnPortId);
   const [stations, setStations] = useState(initialStations);
-
+  const cruiseStatusIdStr = cruiseStatusId.toString();
   const handleNavCruisesList = () => {
     navigate("/cruises");
   };
 
   const handleEnterNewStation = () => {
-    setEnterNewStation(!enterNewStation);
+    setNewStationToggle(!newStationToggle);
   };
 
-  const newStationButton = (statusId, newStationToggle) => {
-    if (statusId.toString() === "3" || statusId.toString() === "5") {
-      return <Button disabled>New Station</Button>
+  const handleEditCruise = () => {
+    setEditCruiseToggle(!editCruiseToggle);
+  };
+
+  const handleSaveCruise = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const values = {
+      id,
+      cruiseName,
+      cruiseStatusId,
+      vesselName,
+      startDate,
+      endDate,
+      departurePortId,
+      returnPortId
+    };
+
+    for (const [key, value] of formData.entries()) {
+      values[key] = value;
     }
-    return newStationToggle
-      ? <Button onClick={handleEnterNewStation} secondary>Cancel New Station</Button>
-      : <Button onClick={handleEnterNewStation}>New Station</Button>
-  }
+
+    const updatedCruise = await put(`${API_BASE_URL}/cruises/${id}`, values);
+    setEditCruiseToggle(!editCruiseToggle);
+    setCruise(updatedCruise);
+  };
 
   const handleNewStation = async (event) => {
     event.preventDefault();
@@ -83,7 +108,7 @@ function CruiseDetailPage({ data }) {
     // process date time
     const newStation = await post(`${API_BASE_URL}/stations`, newValues);
     event.target.reset();
-    setEnterNewStation(!enterNewStation);
+    setNewStationToggle(!newStationToggle);
     setStations([newStation, ...stations]);
   }
 
@@ -92,46 +117,134 @@ function CruiseDetailPage({ data }) {
       <Grid row className="margin-top-2">
         <Button className="margin-right-0" onClick={handleNavCruisesList}>&lt; Cruise List</Button>
       </Grid>
-      <Grid row className="flex-justify">
+      <Grid row className="flex-justify margin-top-2">
         <h1 className="app-sec-header">Cruise Details</h1>
-        <div className="margin-top-2">
-          <Tag className={`usa-tag--big ${setStatusColor(cruiseStatusId)}`}>{cruiseStatus}</Tag>
+        <div className="margin-top-05">
+          <Tag className={`padding-1 usa-tag--big ${setStatusColor(cruiseStatusId)}`}>{cruiseStatus}</Tag>
         </div>
+        <Button
+          disabled={cruiseStatusIdStr === CruiseStatus.SUBMITTED || cruiseStatusIdStr === CruiseStatus.ACCEPTED || newStationToggle}
+          onClick={cruiseStatusIdStr !== CruiseStatus.SUBMITTED && cruiseStatusIdStr !== CruiseStatus.ACCEPTED ? handleEditCruise : undefined}
+          secondary={editCruiseToggle}
+          className="margin-right-0"
+        >
+          {cruiseStatusIdStr === CruiseStatus.SUBMITTED || cruiseStatusIdStr === CruiseStatus.ACCEPTED ? "Edit Cruise" : editCruiseToggle ? "Cancel Edit Cruise" : "Edit Cruise"}
+        </Button>
       </Grid>
-      <div className="border radius-lg padding-1 padding-bottom-0 margin-bottom-2 app-box-shadow">
-        <Grid row>
-          <Grid col={12} tablet={{ col: true }}>
-            <DescriptionListItem term="Cruise Name:" description={cruiseName} />
-          </Grid>
-          <Grid col={12} tablet={{ col: true }}>
-            <DescriptionListItem term="Vessel Name:" description={vesselName} />
-          </Grid>
-        </Grid>
-        <Grid row className="margin-top-2">
-          <Grid col={12} tablet={{ col: true }}>
-            <Fieldset legend="Departure" className="app-legend-bold-text">
-              <DescriptionListItem term="Date:" description={startDate} />
-              <DescriptionListItem term="Port:" description={departurePort} />
-            </Fieldset>
-          </Grid>
-          <Grid col>
-            <Fieldset legend="Return" className="app-legend-bold-text">
-              <DescriptionListItem term="Date:" description={endDate} />
-              <DescriptionListItem term="Port:" description={returnPort} />
-            </Fieldset>
-          </Grid>
-        </Grid>
-        <Grid row className="flex-justify margin-bottom-1">
-          <h2 className="app-sec-header">Stations</h2>
-          {newStationButton(cruiseStatusId, enterNewStation)}
-        </Grid>
-        {enterNewStation && <StationNew handleNewStation={handleNewStation} />}
-        {stations.length
-          ? stations.map((station) => (
-            <StationSummary key={station.id} station={station} />
-          ))
-          : ""}
+      <div className="border radius-lg padding-1 margin-y-2 app-card">
+        {editCruiseToggle
+          ?
+          <Form className="maxw-full" onSubmit={handleSaveCruise}>
+            <Grid row gap>
+              <Grid col={12} tablet={{ col: true }}>
+                <Label htmlFor="cruise-name" className="text-bold margin-top-1" requiredMarker>
+                  Cruise Name:
+                </Label>
+                <TextInput id="cruise-name" name="cruiseName" defaultValue={cruiseName} required />
+              </Grid>
+              <Grid col={12} tablet={{ col: true }}>
+                <Label htmlFor="vessel-name" className="text-bold margin-top-1" requiredMarker>
+                  Vessel Name:
+                </Label>
+                <TextInput id="vessel-name" name="vesselName" defaultValue={vesselName} required />
+              </Grid>
+            </Grid>
+            <Grid row gap className="margin-top-2">
+              <Grid col={12} tablet={{ col: true }}>
+                <Fieldset legend="Departure" className="app-legend-bold-text">
+                  <Label htmlFor="start-date" className="text-bold margin-top-1" requiredMarker>
+                    Date:
+                  </Label>
+                  <DatePicker id="start-date" name="startDate" defaultValue={startDate} required />
+                  <Label htmlFor="departure-port-select" className="text-bold margin-top-1" requiredMarker>
+                    Port:
+                  </Label>
+                  <Select
+                    id="departure-port-select"
+                    name="departurePortId"
+                    defaultValue={departurePortId}
+                    required
+                  >
+                    <option value={null}>- Select Port -</option>
+                    {ports.map((port) => (
+                      <option key={port.id} value={port.id}>
+                        {port.name}
+                      </option>
+                    ))}
+                  </Select>
+                </Fieldset>
+              </Grid>
+              <Grid col>
+                <Fieldset legend="Return" className="app-legend-bold-text">
+                  <Label htmlFor="end-date" className="text-bold margin-top-1" >
+                    Date:
+                  </Label>
+                  <DatePicker id="end-date" name="endDate" defaultValue={endDate} />
+                  <Label htmlFor="return-port-select" className="text-bold margin-top-1" >
+                    Port:
+                  </Label>
+                  <Select
+                    id="return-port-select"
+                    name="returnPortId"
+                    defaultValue={returnPortId}
+                  >
+                    <option value={null}>- Select Port -</option>
+                    {ports.map((port) => (
+                      <option key={port.id} value={port.id}>
+                        {port.name}
+                      </option>
+                    ))}
+                  </Select>
+                </Fieldset>
+              </Grid>
+            </Grid>
+            <Grid row className="flex-column flex-align-end">
+              <Button type="submit" >Save Cruise</Button>
+            </Grid>
+          </Form>
+          : <>
+            <Grid row>
+              <Grid col={12} tablet={{ col: true }}>
+                <DescriptionListItem term="Cruise Name:" description={cruiseName} />
+              </Grid>
+              <Grid col={12} tablet={{ col: true }}>
+                <DescriptionListItem term="Vessel Name:" description={vesselName} />
+              </Grid>
+            </Grid>
+            <Grid row className="margin-top-2">
+              <Grid col={12} tablet={{ col: true }}>
+                <Fieldset legend="Departure" className="app-legend-bold-text">
+                  <DescriptionListItem term="Date:" description={startDate} />
+                  <DescriptionListItem term="Port:" description={departurePort} />
+                </Fieldset>
+              </Grid>
+              <Grid col>
+                <Fieldset legend="Return" className="app-legend-bold-text">
+                  <DescriptionListItem term="Date:" description={endDate} />
+                  <DescriptionListItem term="Port:" description={returnPort} />
+                </Fieldset>
+              </Grid>
+            </Grid>
+          </>
+        }
       </div>
+      <Grid row className="flex-justify margin-bottom-1">
+        <h2 className="app-sec-header">Stations</h2>
+        <Button
+          disabled={cruiseStatusIdStr === CruiseStatus.SUBMITTED || cruiseStatusIdStr === CruiseStatus.ACCEPTED || editCruiseToggle}
+          onClick={cruiseStatusIdStr !== CruiseStatus.SUBMITTED && cruiseStatusIdStr !== CruiseStatus.ACCEPTED ? handleEnterNewStation : undefined}
+          secondary={newStationToggle}
+          className="margin-right-0"
+        >
+          {cruiseStatusIdStr === CruiseStatus.SUBMITTED || cruiseStatusIdStr === CruiseStatus.ACCEPTED ? "New Station" : newStationToggle ? "Cancel New Station" : "New Station"}
+        </Button>
+      </Grid>
+      {newStationToggle && <StationNew handleNewStation={handleNewStation} />}
+      {stations.length
+        ? stations.map((station) => (
+          <StationSummary key={station.id} station={station} />
+        ))
+        : ""}
     </>
   );
 }
