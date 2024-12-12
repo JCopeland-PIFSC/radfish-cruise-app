ebc2b62bcacc5349d5cbe0ef3cd0ead8d031e0af
Jesse Copeland <jesse.copeland@noaa.gov>
2024-12-12 13:01:08 -1000

    Add CatchForm

---
+++ src/components/CatchForm.jsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import { Grid, Form, Label, TextInput, Select, Button } from "@trussworks/react-uswds";
+import SampleInput from "./SampleInput";
+
+const InitializedSample = {
+  "lengthCm": "",
+  "bioSample": {
+    "sampleName": "",
+    "sampleTypeId": "",
+    "notes": ""
+  }
+};
+
+const CatchForm = ({ formData, speciesList, sampleTypesList, onSubmit }) => {
+  const [localData, setLocalData] = useState(structuredClone(formData));
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLocalData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  const handleSampleInputChange = (event, index) => {
+    const { name, value } = event.target;
+
+    setLocalData((prev) => {
+      const updatedIndividuals = [...prev.individuals];
+
+      if (name in updatedIndividuals[index].bioSample) {
+        updatedIndividuals[index] = {
+          ...updatedIndividuals[index],
+          bioSample: {
+            ...updatedIndividuals[index].bioSample,
+            [name]: value,
+          },
+        };
+      } else {
+        updatedIndividuals[index] = {
+          ...updatedIndividuals[index],
+          [name]: value,
+        };
+      }
+
+      return {
+        ...prev,
+        individuals: updatedIndividuals
+      };
+    });
+  };
+
+  const handleAddSample = (individual) => {
+    setLocalData((prev) => ({
+      ...prev,
+      individuals: [individual, ...prev.individuals]
+    }));
+  };
+
+  const handleRemoveSample = (index) => {
+    setLocalData((prev) => ({
+      ...prev,
+      individuals: prev.individuals.filter((_, idx) => idx !== index)
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(localData);
+  };
+
+  return (
+    <Form className="maxw-full" onSubmit={handleSubmit}>
+      <Grid row gap>
+        <Grid col={12} tablet={{ col: true }}>
+          <Grid row>
+            <Label htmlFor="species-select" className="grid-col-4 text-bold margin-top-2" requiredMarker>
+              Species:
+            </Label>
+            <Select
+              id="species-select"
+              name="speciesId"
+              value={localData.speciesId}
+              onChange={handleChange}
+              className="grid-col-8"
+              required
+            >
+              <option value={null}>- Select Species -</option>
+              {speciesList.map((species) => (
+                <option key={species.id} value={species.id}>
+                  {species.name}
+                </option>
+              ))}
+            </Select>
+          </Grid>
+        </Grid>
+        <Grid col={12} tablet={{ col: true }}>
+          <Grid row>
+            <Label htmlFor="catch-weight" className="grid-col-4 text-bold margin-top-2" requiredMarker>
+              Total Weight:
+            </Label>
+            <TextInput
+              id="catch-weight"
+              name="aggregateWeightKg"
+              value={localData.aggregateWeightKg}
+              onChange={handleChange}
+              className="grid-col-8"
+              required />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid row className="flex-justify-end margin-bottom-2">
+        <Button
+          type="button"
+          className="margin-right-0"
+          onClick={() => handleAddSample(structuredClone(InitializedSample))}
+        >Add New Sample</Button>
+      </Grid>
+      <hr className="border-top-width-05 border-base" />
+
+      {localData.individuals.length
+        ? localData.individuals.map((sample, idx) => (
+          <div key={idx}>
+            {(idx > 0) && <hr className="app-hr-light" />}
+            <SampleInput
+              sample={sample}
+              dataIndex={idx}
+              onChange={(event) => handleSampleInputChange(event, idx)}
+              handleRemoveSample={() => handleRemoveSample(idx)}
+              sampleTypesList={sampleTypesList} />
+          </div>
+        ))
+        : <p>No Samples Recorded</p>
+      }
+      <>
+        <hr className="app-hr-light" />
+        <Grid row className="flex-column flex-align-end">
+          <Button type="submit" className="margin-right-0">Save Catch</Button>
+        </Grid>
+      </>
+    </Form >
+  );
+}
+
+export default CatchForm;
