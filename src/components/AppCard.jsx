848060540126d4ee7e16b3981bbfbdcf27aa38dc
Jesse Copeland <jesse.copeland@noaa.gov>
2024-12-23 07:36:00 -1000

    Increase card padding

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 
 const AppCard = forwardRef(({ children, className }, ref) => {
   return (
-    <div ref={ref} className={`1border padding-1 margin-y-2 radius-lg app-card ${className || ""}`}>
+    <div ref={ref} className={`padding-3 margin-y-2 radius-lg app-card ${className || ""}`}>
       {children}
     </div>
   );
