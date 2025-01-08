47b47c67bad3993123161682c657195e56725753
Jesse Copeland <jesse.copeland@noaa.gov>
2025-01-07 15:36:20 -1000

    Simplify how user is set

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { useOfflineStorage } from "@nmfs-radfish/react-radfish";
-import { useStoreUser, useResetCurrentUser } from "../hooks/useUsers";
+import { useStoreUser } from "../hooks/useUsers";
 
 const AuthContext = createContext();
 
@@ -8,48 +8,35 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const { persistNewUser } = useStoreUser();
-  const { resetAndSetCurrentUser } = useResetCurrentUser();
   const { findOne } = useOfflineStorage();
 
-  // Initialize user on mount
-  useEffect(() => {
-    loadUserFromDB();
-  }, []);
+  const login = async (authUserData) => {
+    // Store the new user
+    const newUser = await persistNewUser(authUserData);
+    // Reset other users' isCurrentUser and set the new user
+    // await resetAndSetCurrentUser(newUser.id);
+    try {
+      setUser(authUserData); // Ensure userData is valid
+    } catch (err) {
+      console.error("Error in setUser:", err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // Function to load the current user from IndexedDB
-  const loadUserFromDB = async () => {
-    const storedUser = await findOne("users", { isCurrentUser: 1 });
-    if (storedUser?.isAuthenticated) {
-      setUser(storedUser);
+  const setCurrentUser = async (userId) => {
+    const foundUser = await findOne("users", { where: { id: userId } });
+    if (foundUser?.isAuthenticated) {
+      setUser(foundUser);
     } else {
       setUser(null);
     }
     setLoading(false);
   };
 
-  const login = async (authUserData) => {
-    // Store the new user
-    const newUser = await persistNewUser(authUserData);
-    // Reset other users' isCurrentUser and set the new user
-    await resetAndSetCurrentUser(newUser.id);
-    try {
-      setUser(authUserData); // Ensure userData is valid
-    } catch (err) {
-      console.error("Error in setUser:", err);
-      throw err;
-    }
-  };
-
-  const logout = async () => {
-    await updateUser({
-      userId: user.id,
-      updates: { isAuthenticated: 0, isCurrent: 0 },
-    });
-    setUser(null);
-  };
-
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading, loadUserFromDB }}>
+    <AuthContext.Provider value={{ user, login, loading, setCurrentUser }}>
       {children}
     </AuthContext.Provider>
   );
