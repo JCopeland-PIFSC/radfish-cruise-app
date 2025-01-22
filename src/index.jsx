b43c8518cd73b635942d80455f9a274899df888b
Jesse Copeland <jesse.copeland@noaa.gov>
2025-01-22 12:21:33 -1000

    Remove ReactQuery Dev Tools

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import "./styles/theme.css";
 import App from "./App";
 import { Application } from "@nmfs-radfish/radfish";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import store from "./db/store.js";
 import { tablesMetadataSeed as seed } from "./db/seeds.js";
 import { initMetadataTable } from "./utils/databaseHelpers.js";
@@ -52,7 +51,6 @@ app.on("ready", async () => {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App application={app} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </React.StrictMode>,
   );
