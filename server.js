7155096f86e1e73871ba9419d52a7550b2bddd22
Tony Gaskell <tony@sudokrew.com>
2025-02-04 15:34:00 -1000

    Fix authenticated value on server to work with client logic

--- server.js
+++ server.js
@@ -7,30 +7,32 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.post("/login", (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    if (!username || !password) {
-      return res.status(400).json({ error: "username and password are required" });
-    }
-  
-    // Access the `users` table in db.json
-    const users = router.db.get("users").value();
-  
-    // Find the user with the matching username and password
-    const user = users.find(
-      (user) => user.username === username && user.password === password
-    );
-  
-    if (!user) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-  
-    // If user exists, send success response with user data (e.g., token)
-    return res.status(200).json({
-      message: "Login successful",
-      user: { id: user.id, username: user.username, isAuthenticated: true },
-    });
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "username and password are required" });
+  }
+
+  // Access the `users` table in db.json
+  const users = router.db.get("users").value();
+
+  // Find the user with the matching username and password
+  const user = users.find(
+    (user) => user.username === username && user.password === password,
+  );
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  // If user exists, send success response with user data (e.g., token)
+  return res.status(200).json({
+    message: "Login successful",
+    user: { id: user.id, username: user.username, isAuthenticated: 1 },
   });
+});
 
 server.post("/api/cruises", (req, res) => {
   const { data } = req.body;
